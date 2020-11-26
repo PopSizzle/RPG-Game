92b9892e629023411082cbc1c6c9730383694b8f
Sam Poppe <poppe.samuel@gmail.com>
2020-11-25 21:12:12 -0800

    adding level 1 monsters

---
+++ Monsters/level1Monsters.js
@@ -0,0 +1,34 @@
+const lvl1Monsters = [
+  {
+    name: 'Rat',
+    HP: 10,
+    physAttack: 1,
+    magAttack: 0,
+    physDef: 1,
+    magDef: 1,
+    xp: 5,
+    lootRating: 1
+  },
+  {
+    name: 'Goblin',
+    HP: 20,
+    physAttack: 2,
+    magAttack: 1,
+    physDef: 2,
+    magDef: 2,
+    xp: 5,
+    lootRating: 5
+  },
+  {
+    name: 'Spider',
+    HP: 30,
+    physAttack: 4,
+    magAttack: 0,
+    physDef: 3,
+    magDef: 1,
+    xp: 10,
+    lootRating: 3
+  }
+]
+
+module.exports=lvl1Monsters;
